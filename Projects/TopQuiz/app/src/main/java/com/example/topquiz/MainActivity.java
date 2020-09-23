e08a38580eb9fe5c5515e22d2058ac200bcd18b6
Romain <33251516+Jesus974@users.noreply.github.com>
2020-09-23 11:22:42 +0200

    TopQuizz

--- Projects/TopQuiz/app/src/main/java/com/example/topquiz/MainActivity.java
+++ Projects/TopQuiz/app/src/main/java/com/example/topquiz/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.topquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mPlaybutton;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Disable Play Button
+        mPlaybutton = findViewById(R.id.button);
+        mPlaybutton.setEnabled(false);
+
+        // Initialisation Edit Text
+        mEditText = findViewById(R.id.editTextTextPersonName);
+
+        // Ajout d'un listener
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPlaybutton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
